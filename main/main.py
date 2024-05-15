732abe84425ebba0cf1f6e84471f6a5a905deb76
Falko-droid <75842618+Falko-droid@users.noreply.github.com>
2024-05-15 10:53:00 +0700

    Need changes

--- main/main.py
+++ main/main.py
@@ -1,9 +1,39 @@
-number = 123
-num = number + 231
-print(num)
-if number >= num:
-    print("Hello")
-else:
-    print("Good bye")
+def is_film_exist(move, films):
+    for i_move in films:
+        if i_move == move:
+            return True
+    return False
 
-sum = 322
+films = [
+         'Крепкий орешек', 'Назад в будущее', 'Таксист',
+         'Леон', 'Богемская рапсодия', 'Город грехов',
+         'Мементо', 'Отступники', 'Деревня',
+         'Проклятый остров', 'Начало', 'Матрица'
+]
+
+my_list = []
+while True:
+    print("\nВаш текущий топ фильмов:", my_list)
+    move = input("Введите название фильма: ")
+    if is_film_exist(move, films):
+        decision = input("Команды: добавить, вставить, удалить: ")
+        if decision == "добавить":
+            if is_film_exist(move, my_list):
+                print("Этот фильм уже есть в вашем списке")
+            else:
+                my_list.append(move)
+        elif decision == "вставить":
+            if is_film_exist(move, my_list):
+                print("Этот фильм уже есть в вашем списке")
+            else:
+                index = int(input("На какое место: "))
+                my_list.insert(index - 1, move)
+        elif decision == "удалить":
+            if is_film_exist(move, my_list):
+                my_list.remove(move)
+            else:
+                print("Этого фильма нет в вашем списке")
+        else:
+            print("Неверная команда")
+    else:
+        print("Данного фильма нет в списке")
